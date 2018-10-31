61b90639f581ddea3c13648e8ae9381682bf8fc8
ankitsaxenajhs <38401765+ankitsaxenajhs@users.noreply.github.com>
2018-10-31 12:40:19 +0530

    create sequenceequation.cpp
    solution of sequenceequation

---
+++ Algorithms/Implementation/Sequenceequation.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int p[100];
+    for (int i = 1; i <= n; ++i)
+        cin >> p[i];
+    int ans[100];
+    for (int i = 1; i <= n; ++i)
+        ans[p[p[i]]] = i;
+    for (int i = 1; i <= n; ++i)
+        cout << ans[i] << '\n';
+}
