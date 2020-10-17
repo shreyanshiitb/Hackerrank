cefe796fcc695e73e721aaaed090691550946951
maneesh1510 <71519887+maneesh1510@users.noreply.github.com>
2020-10-17 12:42:23 +0530

    Update quicksort1.cpp
    change code for quicksort1.cpp because previous algorithm gives wrong answer and also reduces space complexity by using only ne array instead of two array.

--- Algorithms/Sorting/quicksort1.cpp
+++ Algorithms/Sorting/quicksort1.cpp
@@ -2,23 +2,43 @@
 #include<iostream>
 using namespace std;
 
-int a[1000], b[1000], nb;
+int partition(int a[],int p,int r){
+	int x=a[r],temp;
+	int i=p-1;
+	for(int j=p;j<r;j++){
+	if(a[j]<=x)
+	{
+		i=i+1;
+		temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+		
+	}
+	}
+	a[r]=a[i+1];
+	a[i+1]=x;
+	return (i+1);
+}
 
+void quicksort(int a[],int p,int r){
+	if(p<r){
+		int q=partition(a,p,r);
+		quicksort(a,p,q-1);
+		quicksort(a,q+1,r);
+	}
+ 
+}
+	
 int main()
 {
 	int n;
 	cin >> n;
+	int a[n];
+	for (int i = 0; i < n; i++){
+		cin >> a[i];}
+	quicksort(a,0,n-1);
 	for (int i = 0; i < n; i++)
-		cin >> a[i];
-	for (int i = 1; i < n; i++)
-		if (a[i] < a[0])
-			b[nb++] = a[i];
-	b[nb++] = a[0];
-	for (int i = 1; i < n; i++)
-		if (a[i] > a[0])
-			b[nb++] = a[i];
-	for (int i = 0; i < n; i++)
-		cout << b[i] << " ";
-	cout << endl;
+		cout << a[i] << " ";
+	
 	return 0;
 }
